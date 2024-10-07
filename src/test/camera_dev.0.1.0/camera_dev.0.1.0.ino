09b71345b645284f0b4154504481d9eea0b275cd
airpocket-soundman <yamashita.3154@gmail.com>
2024-10-07 20:08:10 +0900

    test

--- src/test/camera_dev.0.1.0/camera_dev.0.1.0.ino
+++ src/test/camera_dev.0.1.0/camera_dev.0.1.0.ino
@@ -1,243 +1,90 @@
-#include <SDHCI.h>
-#include <stdio.h>  /* for sprintf */
+/*
+ *  Spresense_camera_take_picture.ino - Simple digital camera application
+ *  Copyright 2019 Sony Semiconductor Solutions Corporation
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 
 #include <Camera.h>
-#define BAUDRATE                (115200)
-#define TOTAL_PICTURE_COUNT     (1)
-
-SDClass  theSD;
-int take_picture_count = 0;
-int value=0;
-bool CamReady=true;
-bool CamDone=false;
-const int DIN_PIN = 4;
-
-
-//TFT設定
 #include <SPI.h>
+#include <SDHCI.h>
+#include "Adafruit_GFX.h"
 #include "Adafruit_ILI9341.h"
 
-/* LCD Settings */
 #define TFT_RST 8
 #define TFT_DC  9
 #define TFT_CS  10
 
-Adafruit_ILI9341 tft = Adafruit_ILI9341(&SPI, TFT_DC, TFT_CS, TFT_RST); 
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS ,TFT_DC ,TFT_RST);
 
+SDClass theSD;
+bool bButtonPressed = false;
+int gCounter = 0;
 
-void putStringOnLcd(String str, int color) {
-  int len = str.length();
-  tft.fillRect(0,224, 320, 240, ILI9341_BLACK);
-  tft.setTextSize(2);
-  int sx = 160
-  - len/2*12;
-  if (sx < 0) sx = 0;
-  tft.setCursor(sx, 225);
-  tft.setTextColor(color);
-  tft.println(str);
+void changeState () {
+  bButtonPressed = true;
 }
 
-/**
- * Print error message
- */
-
-void printError(enum CamErr err)
-{
-  Serial.print("Error: ");
-  switch (err)
-    {
-      case CAM_ERR_NO_DEVICE:
-        Serial.println("No Device");
-        break;
-      case CAM_ERR_ILLEGAL_DEVERR:
-        Serial.println("Illegal device error");
-        break;
-      case CAM_ERR_ALREADY_INITIALIZED:
-        Serial.println("Already initialized");
-        break;
-      case CAM_ERR_NOT_INITIALIZED:
-        Serial.println("Not initialized");
-        break;
-      case CAM_ERR_NOT_STILL_INITIALIZED:
-        Serial.println("Still picture not initialized");
-        break;
-      case CAM_ERR_CANT_CREATE_THREAD:
-        Serial.println("Failed to create thread");
-        break;
-      case CAM_ERR_INVALID_PARAM:
-        Serial.println("Invalid parameter");
-        break;
-      case CAM_ERR_NO_MEMORY:
-        Serial.println("No memory");
-        break;
-      case CAM_ERR_USR_INUSED:
-        Serial.println("Buffer already in use");
-        break;
-      case CAM_ERR_NOT_PERMITTED:
-        Serial.println("Operation not permitted");
-        break;
-      default:
-        break;
+void CamCB(CamImage img){
+  if (img.isAvailable()) {
+    img.convertPixFormat(CAM_IMAGE_PIX_FMT_RGB565);
+    tft.drawRGBBitmap(0, 0, (uint16_t *)img.getImgBuff(), 320, 240);
+    if (bButtonPressed) {
+      tft.setTextSize(2); 
+      tft.setCursor(100, 200);
+      tft.setTextColor(ILI9341_RED);
+      tft.println("Shooting");
     }
-}
-
-/**
- * Callback from Camera library when video frame is captured.
- */
-
-void CamCB(CamImage img)
-{
-
-  /* Check the img instance is available or not. */
-
-  if (img.isAvailable())
-    {
-
-      /* If you want RGB565 data, convert image data format to RGB565 */
-
-      img.convertPixFormat(CAM_IMAGE_PIX_FMT_RGB565);
-
-      Serial.print("Image data size = ");
-      Serial.print(img.getImgSize(), DEC);
-      Serial.print(" , ");
-
-      Serial.print("buff addr = ");
-      Serial.print((unsigned long)img.getImgBuff(), HEX);
-      Serial.println("");
-
-    uint16_t* imgBuf = (uint16_t*)img.getImgBuff(); 
-
-    String gStrResult = "Ready";
-    if(CamDone){
-      gStrResult = "PIC=OK";
-      CamDone=false;
-    }
-    //TFT描画
-    tft.drawRGBBitmap(0, 0, (uint16_t *)img.getImgBuff(), 320, 224); 
-    putStringOnLcd(gStrResult, ILI9341_YELLOW);
-    }
-  else
-    {
-      Serial.println("Failed to get video stream image");
-    }
-}
-
-/**
- * @brief Initialize camera
- */
-void setup()
-{
-  //SW用
-  pinMode( DIN_PIN, INPUT_PULLUP );
-
-  //TFT
-  tft.begin();
-  tft.setRotation(1);
-  
-  CamErr err;
-
-  /* Open serial communications and wait for port to open */
-
-  Serial.begin(BAUDRATE);
-  while (!Serial)
-    {
-      ; /* wait for serial port to connect. Needed for native USB port only */
-    }
-
-  /* Initialize SD */
-  while (!theSD.begin()) 
-    {
-      /* wait until SD card is mounted. */
-      Serial.println("Insert SD card.");
-    }
-
-  /* begin() without parameters means that
-   * number of buffers = 1, 30FPS, QVGA, YUV 4:2:2 format */
-
-  Serial.println("Prepare camera");
-  err = theCamera.begin();
-  if (err != CAM_ERR_SUCCESS)
-    {
-      printError(err);
-    }
-
-  /* Start video stream.
-   * If received video stream data from camera device,
-   *  camera library call CamCB.
-   */
-
-  Serial.println("Start streaming");
-  err = theCamera.startStreaming(true, CamCB);
-  if (err != CAM_ERR_SUCCESS)
-    {
-      printError(err);
-    }
-
-  /* Auto white balance configuration */
-
-  Serial.println("Set Auto white balance parameter");
-  err = theCamera.setAutoWhiteBalanceMode(CAM_WHITE_BALANCE_DAYLIGHT);
-  if (err != CAM_ERR_SUCCESS)
-    {
-      printError(err);
-    }
- 
-  Serial.println("Set still picture format");
-  err = theCamera.setStillPictureImageFormat(
-     CAM_IMGSIZE_QUADVGA_H,
-     CAM_IMGSIZE_QUADVGA_V,
-     CAM_IMAGE_PIX_FMT_JPG);
-  if (err != CAM_ERR_SUCCESS)
-    {
-      printError(err);
-    }
-    theCamera.startStreaming(true, CamCB);
-}
-
-void CheckSW(){
-
-  value = digitalRead( DIN_PIN );
-  //Serial.println( value );
-  if(value == 0){
-    CamReady=true;
-    CamDone=true;
   }
 }
 
-void loop()
-{
-  sleep(0.5);
-  CheckSW();
+int intPin = 4;
+void setup() {
+  pinMode(LED0, OUTPUT);
+  pinMode(intPin, INPUT_PULLUP);
 
-  if(CamReady)
-  {
-  
-      Serial.println("call takePicture()");
-      CamImage img = theCamera.takePicture();
+  theSD.begin();
+  Serial.begin(115200);
+  tft.begin();
+  tft.setRotation(3);
+  theCamera.begin();
+  theCamera.startStreaming(true, CamCB);
 
-      if (img.isAvailable())
-        {
-          /* Create file name */
-    
-          char filename[16] = {0};
-          sprintf(filename, "PICT_%03d.JPG", take_picture_count);
-    
-          Serial.print("Save taken picture as ");
-          Serial.print(filename);
-          Serial.println("");
+  theCamera.setStillPictureImageFormat(
+    CAM_IMGSIZE_QVGA_H ,CAM_IMGSIZE_QVGA_V
+   ,CAM_IMAGE_PIX_FMT_JPG);
 
-          theSD.remove(filename);
-          File myFile = theSD.open(filename, FILE_WRITE);
-          myFile.write(img.getImgBuff(), img.getImgSize());
-          myFile.close();
+  attachInterrupt(digitalPinToInterrupt(intPin) ,changeState ,FALLING);
+}
 
-          take_picture_count++;
-        }
-      else
-        {
-          Serial.println("Failed to take picture");
-        }
-        CamReady=false;
-
-    }
+void loop() {
+  if (!bButtonPressed) return;
+  Serial.println("button pressed");
+  digitalWrite(LED0, HIGH);
+  theCamera.startStreaming(false, CamCB);  // streaming off
+  CamImage img = theCamera.takePicture();
+  if (img.isAvailable()) {
+      char filename[16] = {0};
+      sprintf(filename, "PICT%03d.JPG", gCounter);
+      File myFile = theSD.open(filename,FILE_WRITE);
+      myFile.write(img.getImgBuff(), img.getImgSize());
+      myFile.close();
+      ++gCounter;
+  }
+  bButtonPressed = false;
+  theCamera.startStreaming(true, CamCB);  // streaming on
+  digitalWrite(LED0, LOW);
 }
