3e6523a275db3bf46265116fce03b2646805c03e
airpocket-soundman <yamashita.3154@gmail.com>
2024-10-09 17:11:08 +0900

    学習用画像追加

--- src/test/camera_dev.0.1.0/camera_dev.0.1.0.ino
+++ src/test/camera_dev.0.1.0/camera_dev.0.1.0.ino
@@ -38,21 +38,74 @@ void changeState () {
   bButtonPressed = true;
 }
 
+void printError(enum CamErr err)
+{
+  Serial.print("Error: ");
+  switch (err)
+  {
+      case CAM_ERR_NO_DEVICE:
+    Serial.println("No Device");
+    break;
+      case CAM_ERR_ILLEGAL_DEVERR:
+    Serial.println("Illegal device error");
+    break;
+      case CAM_ERR_ALREADY_INITIALIZED:
+    Serial.println("Already initialized");
+    break;
+      case CAM_ERR_NOT_INITIALIZED:
+    Serial.println("Not initialized");
+    break;
+      case CAM_ERR_NOT_STILL_INITIALIZED:
+    Serial.println("Still picture not initialized");
+    break;
+      case CAM_ERR_CANT_CREATE_THREAD:
+    Serial.println("Failed to create thread");
+    break;
+      case CAM_ERR_INVALID_PARAM:
+    Serial.println("Invalid parameter");
+    break;
+      case CAM_ERR_NO_MEMORY:
+    Serial.println("No memory");
+    break;
+      case CAM_ERR_USR_INUSED:
+    Serial.println("Buffer already in use");
+    break;
+      case CAM_ERR_NOT_PERMITTED:
+    Serial.println("Operation not permitted");
+    break;
+      default:
+    break;
+  }
+}
+
 void CamCB(CamImage img){
   if (img.isAvailable()) {
     img.convertPixFormat(CAM_IMAGE_PIX_FMT_RGB565);
     tft.drawRGBBitmap(0, 0, (uint16_t *)img.getImgBuff(), 320, 240);
+    Serial.print("Image data size = ");
+    Serial.print(img.getImgSize(), DEC);
+    Serial.print(" , ");
+    Serial.print("buff addr = ");
+    Serial.print((unsigned long)img.getImgBuff(), HEX);
+    Serial.println("");
     if (bButtonPressed) {
       tft.setTextSize(2); 
       tft.setCursor(100, 200);
       tft.setTextColor(ILI9341_RED);
-      tft.println("Shooting");
+      //tft.println("Shooting");
+      char filename[16] = {0};
+      sprintf(filename, "PICT%03d.JPG", gCounter);
+      tft.println(filename);
     }
+  } else {
+    Serial.println("Failed to get video stream image");
   }
+
 }
 
 int intPin = 4;
 void setup() {
+  CamErr err;
   pinMode(LED0, OUTPUT);
   pinMode(intPin, INPUT_PULLUP);
 
@@ -60,8 +113,18 @@ void setup() {
   Serial.begin(115200);
   tft.begin();
   tft.setRotation(3);
-  theCamera.begin();
-  theCamera.startStreaming(true, CamCB);
+  Serial.println("Prepare camera");
+  err = theCamera.begin();
+  if (err != CAM_ERR_SUCCESS)
+  {
+      printError(err);
+  }
+  Serial.println("Start streaming");
+  err = theCamera.startStreaming(true, CamCB);
+  if (err != CAM_ERR_SUCCESS)
+  {
+      printError(err);
+  }
 
   theCamera.setStillPictureImageFormat(
     CAM_IMGSIZE_QVGA_H ,CAM_IMGSIZE_QVGA_V
---
+++ src/test/dev.0.3.0/config.h
@@ -0,0 +1,46 @@
+/*
+ *  config.h - WiFi Configration Header
+ *
+ *  This work is free software; you can redistribute it and/or modify it under the terms 
+ *  of the GNU Lesser General Public License as published by the Free Software Foundation; 
+ *  either version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This work is distributed in the hope that it will be useful, but without any warranty; 
+ *  without even the implied warranty of merchantability or fitness for a particular 
+ *  purpose. See the GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License along with 
+ *  this work; if not, write to the Free Software Foundation, 
+ *  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+/*-------------------------------------------------------------------------*
+ * Configration
+ *-------------------------------------------------------------------------*/
+#define HOME //HOME or WORK
+
+#ifdef HOME
+#define  AP_SSID        "kumakero2.4"
+#define  PASSPHRASE     "4roses6126"
+#define  HTTP_SRVR_IP   "192.168.50.104"
+#define  MQTT_SRVR      "192.168.50.104"
+
+#else
+#define  AP_SSID        "J00WLN1305A"
+#define  PASSPHRASE     "m1n0ru0869553434@LAN"
+#define  HTTP_SRVR_IP   "192.168.1.65"
+#define  MQTT_SRVR      "192.168.1.65"
+#endif
+
+#define  HTTP_PORT      "1880"
+#define  HTTP_GET_PATH  "/getdata"
+#define  HTTP_POST_PATH "/postdata"
+
+#define  MQTT_PORT     "1883"
+#define  MQTT_CLI_ID   "Telit_Device_pub"
+#define  MQTT_TOPIC1   "fromNodeRed"
+
+#endif /*_CONFIG_H_*/
